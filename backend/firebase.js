c5f7a241d4d22792620a7fff6937af5678988213
mouad akroubi <mouad akroubi@gmail.com>
2024-02-22 16:11:25 +0100

    switch to my accounts setting

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,13 +2,12 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApO6vuHixlRFZ87qaf-ZBXOK0O-6k6rbE",
-  authDomain: "audio-e1a96.firebaseapp.com",
-  projectId: "audio-e1a96",
-  storageBucket: "audio-e1a96.appspot.com",
-  messagingSenderId: "570092642508",
-  appId: "1:570092642508:web:7d510bfac1b84344aba269",
-  measurementId: "G-FKECEPG5MR"
+  apiKey: "AIzaSyAiaN_Oh3ZkwSAxZSpg-kCNTn3vuhG4s-M",
+  authDomain: "react-app-cb9cb.firebaseapp.com",
+  projectId: "react-app-cb9cb",
+  storageBucket: "react-app-cb9cb.appspot.com",
+  messagingSenderId: "963042786044",
+  appId: "1:963042786044:web:6deff729e8c98a25e77579",
 };
 
 // Initialize Firebase
